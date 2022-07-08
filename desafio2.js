883f1a0e9e1d7136c5bb3dc949c7496d6d33996f
nicolas bronzini <nicolasbronzini7@gmail.com>
2022-07-07 23:15:45 -0300

    arrays

--- desafio2.js
+++ desafio2.js
@@ -1,5 +1,22 @@
 
+function nombre_deudor (usuarios , nombre_usuario){
 
+  
+
+  if ( nombre_usuario = usuarios){
+
+  let deuda = parseInt(prompt("ingrese de que valor es su deuda"));
+
+  let cuota = parseInt(prompt("ingrese en cuabntas cuotas quiere hacer el pago de su deuda"));  
+
+  calcular_pago (nombre_usuario , usuarios)
+
+  } else {
+
+      alert ("Usted no es un deudor" , nombre_usuario)
+  }
+
+};
 
 function calcular_pago( deuda , cuota){
 
@@ -9,19 +26,17 @@ function calcular_pago( deuda , cuota){
 
       total = deuda / cuota
    
-   
+};
 
-}
+let nombre_usuario = prompt("ingrese su nombre");
 
+let usuarios = ["Pepe", "Jose" , "Cata" , "Male"];
 
-
-let deuda = parseInt(prompt("ingrese de que valor es su deuda"));
-
-let cuota = parseInt(prompt("ingrese en cuabntas cuotas quiere hacer el pago de su deuda"));
-
-calcular_pago(deuda , cuota)
+nombre_deudor(usuarios , nombre_usuario);
 
 console.log ("Bienvenido, Pague su deuda");
+
+console.log ("Su nombre es :" , nombre_usuario);
 
 console.log ("El total a pagar es:", total);
 
