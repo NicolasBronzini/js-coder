fa176672cc57c90e0aee808da4716c6a44b11d51
nicolas bronzini <nicolasbronzini7@gmail.com>
2022-07-14 11:14:24 -0300

    input

--- desafio.js
+++ desafio.js
@@ -1,12 +1,13 @@
-function recibir_nombre(){
+btn_nombre.addEventListener("click", function (){
 
   let nombre_usuario = document.getElementById("ingreso_nombre") ;
 
   nombre_deudor (usuarios , nombre_usuario)
-}
+ });
+
 function nombre_deudor (usuarios , nombre_usuario){
 
-  if (usuarios.includes (nombre_usuario)){
+  if (usuarios.includes (nombre_usuario.value)){
 
   let deuda = parseInt(prompt("ingrese de que valor es su deuda"));
 
@@ -26,10 +27,10 @@ function calcular_pago( deuda , cuota){
      if (cuota <=2){
       deuda = deuda *1.21
     }
-
       total = deuda / cuota
    
 };
+
 
 
 let usuarios = ["Pepe", "Jose" , "Cata" , "Male"];
